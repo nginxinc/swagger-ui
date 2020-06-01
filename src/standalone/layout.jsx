7d40b14ab6164d85cb93574e3a8d04da9bee9633
Igor Ippolitov <iippolitov@gmail.com>
2020-06-01 15:04:15 +0100

    merging with master

--- src/standalone/layout.jsx
+++ src/standalone/layout.jsx
@@ -22,7 +22,6 @@ export default class StandaloneLayout extends React.Component {
     let Row = getComponent("Row")
     let Col = getComponent("Col")
 
-    const Topbar = getComponent("Topbar", true)
     const BaseLayout = getComponent("BaseLayout", true)
     const OnlineValidatorBadge = getComponent("onlineValidatorBadge", true)
 
@@ -30,8 +29,22 @@ export default class StandaloneLayout extends React.Component {
     return (
 
       <Container className='swagger-ui'>
-        {Topbar ? <Topbar /> : null}
-        <BaseLayout /> 
+        { loadingStatus === "loading" &&
+          <div className="info">
+            <h4 className="title">Loading...</h4>
+          </div>
+        }
+        { loadingStatus === "failed" &&
+          <div className="info">
+            <h4 className="title">Failed to load spec.</h4>
+          </div>
+        }
+        { loadingStatus === "failedConfig" &&
+          <div className="info" style={{ maxWidth: "880px", marginLeft: "auto", marginRight: "auto", textAlign: "center" }}>
+            <h4 className="title">Failed to load config.</h4>
+          </div>
+        }
+        { !loadingStatus || loadingStatus === "success" && <BaseLayout /> }
         <Row>
           <Col>
             <OnlineValidatorBadge />
